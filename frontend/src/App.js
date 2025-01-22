ec20582bd166c2bbda7203757d92c58b8002c693
aarjookhand <03aarjoo@gmail.com>
2025-01-22 12:00:16 +0100

    chore: initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>docerry is currently under production</h1>
+
+    </div>
+  );
+}
+
+export default App;
