52d06b13eb24f2e93bb8249ca3dc1ccb74ebf02a
aarjookhand <03aarjoo@gmail.com>
2025-01-22 15:04:52 +0100

    feat: about page and contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import WelcomePage from './views/WelcomePage';
+import AboutPage from './views/AboutPage';
+import ContactPage from './views/ContactPage';
 
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/views/AboutPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PublicNavbar from '../components/PublicNavbar'
+import PricingPlans from '../components/PricingPlans'
+
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen bg-[#F9FFFF]">
+      <PublicNavbar />
+      <div className="max-w-4xl mx-auto py-12 px-6 text-center">
+        <h1 className="text-4xl font-bold mb-8 text-gray-800">
+          Welcome to Docerry
+        </h1>
+        <p className="text-lg leading-relaxed text-gray-700 mb-8">
+          Where innovation meets collaboration!
+        </p>
+        <div className="text-left space-y-6 text-gray-800">
+          <p>
+            Say hello to your team’s ultimate partner in building and maintaining a dynamic knowledge base. Designed for today’s agile teams, our platform turns documenting, searching, and sharing technical solutions into a seamless and rewarding experience.
+          </p>
+          <p>
+            At Docerry, we don’t just simplify workflows—we empower teams to shine. Whether you’re tackling complex errors, capturing best practices, or fostering cross-department collaboration, our platform keeps your organization’s knowledge at your fingertips—structured, accessible, and always evolving.
+          </p>
+          <p className="font-medium text-gray-900 flex items-center justify-center">
+            Ready to take your teamwork to the next level? Let’s build smarter, together!
+          </p>
+        </div>
+      </div>
+      <PricingPlans/>
+    </div>
+  )
+}
