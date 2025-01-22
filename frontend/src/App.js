7126c77efdec44550cfa4b0a69b892f3d53fee47
aarjookhand <03aarjoo@gmail.com>
2025-01-22 13:57:16 +0100

    feat: signin page and signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import WelcomePage from './views/WelcomePage';
-
+import SignUpPage from './views/SignUpPage';
+import SignInPage from './views/SignInPage';
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
